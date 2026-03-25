3ca61e41528b08a64fd0cf6ef42902bdc898c28e
SparshSingh0506 <sparshsingh562006@gmail.com>
2026-03-26 00:42:56 +0530

    rewrite: fresh core code

--- components/Card.jsx
+++ components/Card.jsx
@@ -3,7 +3,7 @@ export const Card = ({ card, onClick }) => { /*onClick is the function datatype 
     return (
         <div className = {`card ${(card.isFlipped) ? "flipped" : "" }`}> 
             <div className = "card-front" onClick = {() => onClick(card)}>?</div> 
-            {/*onClick calls the onClick function itself with the respective paramter, which is handCardClick(card)*/}
+            {/*onClick calls the onClick function itself with the respective parameter, which is handCardClick(card)*/}
             <div className = "card-back">{card.value}</div>
         </div>
     );
--- src/App.jsx
+++ src/App.jsx
@@ -3,57 +3,34 @@ import { cardValues } from "../data/cardValues";
 import { Card } from "../components/Card";
 import { useEffect, useState } from "react";
 
+shuffleCards = () => {
+  return;
+}
+
+createCardData = () => {
+  return cardValues.map((value, index) => ({
+    id: index,
+    value,
+    isFlipped: false,
+    isMatched: false,
+  }));
+}
+
 function App() {
   const [cards, setCards] = useState([]);
-  const [flippedCards, setFlippedCards] = useState([]);
-  const [matchedCards, setMatchedCards] = useState([]);
+  const [selectedCards, setSelectedCards] = useState([]);
 
   const initGame = () => {
-    // Shuffle logic
-
-    const finalCards = cardValues.map((value, index) => ({
-      id: index,
-      value,
-      isFlipped: false,
-      isMatched: false,
-    }));
+    shuffleCards();
+    const finalCards = createCardData();
 
     setCards(finalCards);
   }
 
-  useEffect(() => { initGame(); }, []); // empty dependency array means this effect runs only once on mount
+  useEffect(() => initGame(), []);
 
-  const handleCardClick = (card) => {
-    if (card.isFlipped || card.isMatched) return;
-
-    const newCards = cards.map((thisCard) => (thisCard.id === card.id) ? {...thisCard, isFlipped: true} : thisCard)
-    // using spread operator to keep all values same and only update isFlipped attribute for the passed card which was clicked
-    // since its not enclosed under {}, its an implicit return
-
-    setCards(newCards);
-
-    const newFlippedCards = [...flippedCards, card.id];
-    setFlippedCards(newFlippedCards); // Note - even when this is called to schedule the variable with a new value, the next lines reading the flippedCards variable will still use the old value until react re renders the segment
-
-    if (flippedCards.length === 1) { // this will not run for the 1st flip as the flippedCards will still be []. On the 2nd flip, then this will run as the new state will have data from previous clicked card.
-      // when this block runs, its when the 1st card is stored in the flippedCards and this function is being called for the 2nd card click
-      const firstCard = cards[flippedCards[0]];
-
-      if (firstCard.value === card.value) { // match if first clicked card's val matches the next passed card's val on next clicked
-        setMatchedCards((prev) => [...prev, firstCard.id, card.id]); // can directly use functional state updation (using prev state)
-      } 
-      
-      else {
-        setTimeout(() => { // to not instantly flip the flipped cards after the second card is clicked
-          const flippedBackCard = newCards.map((thisCard) => (newFlippedCards.includes(thisCard.id) || thisCard.id === card.id) ? {...thisCard, isFlipped: false}: thisCard); 
-          // flip back both cards to front if cards did not match or if same card is pressed twice
-
-          setCards(flippedBackCard);
-          setFlippedCards([]);
-        }, 800);
-      }
-
-    }
+  const handleCardClick = (clickedCard) => {
+    
   }
 
   return (
