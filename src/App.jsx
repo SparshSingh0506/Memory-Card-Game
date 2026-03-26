66d033a283c0949d03bb6c6b520fb78217bddb0c
SparshSingh0506 <sparshsingh562006@gmail.com>
2026-03-26 18:35:51 +0530

    change logic

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,8 @@ createCardData = () => {
 
 function App() {
   const [cards, setCards] = useState([]);
-  const [selectedCards, setSelectedCards] = useState([]);
+  const [firstCard, setFirstCard] = useState(null);
+  const [isBoardLocked, setIsBoardLocked] = useState(false);
 
   const initGame = () => {
     shuffleCards();
@@ -30,15 +31,47 @@ function App() {
   useEffect(() => initGame(), []);
 
   const handleCardClick = (clickedCard) => {
-    
+    if (clickedCard.isFlipped || isBoardLocked || (clickedCard.id === firstCard?.id)) return;
+
+    setCards(currentCards => currentCards.map(card => (card.id === clickedCard.id) ? { ...card, isFlipped: true } : card));
+
+    if (!firstCard) setFirstCard(clickedCard);
+
+    else { // if its not the first card being clicked, apply matching logic
+      if (firstCard.value === clickedCard.value) {
+        const matchCards = () => {
+          setCards(currentCards =>
+            currentCards.map(card =>
+              (card.id === firstCard.id || card.id === clickedCard.id) ? { ...card, isMatched: true } : card
+            )
+          )
+        }
+        
+        setTimeout(matchCards, 300);
+      }
+
+      else {
+        const resetCards = () => {
+          setCards(currentCards =>
+            currentCards.map(card =>
+              (card.id === firstCard.id || card.id === clickedCard.id) ? { ...card, isFlipped: false } : card
+            )
+          )
+        };
+
+        setTimeout(resetCards, 800);
+      }
+
+      setFirstCard(null);
+    }
   }
 
   return (
-    <div className = "app">
-      <GameHeader score = {3} moves = {5}/>
+    <div className="app">
+      <GameHeader score={3} moves={5} />
 
-      <div className = "cards-grid">
-        {cards.map((card) => <Card card = {card} onClick = {handleCardClick}/>)}
+      <div className="cards-grid">
+        {cards.map(card => <Card card={card} onClick={handleCardClick} />)}
       </div>
     </div>
   );
