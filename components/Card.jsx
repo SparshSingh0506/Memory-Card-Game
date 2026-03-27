ff9f39bebca8e3c1c96a92c0c138dd962b78a094
SparshSingh0506 <sparshsingh562006@gmail.com>
2026-03-27 22:27:08 +0530

    match card ui logic

--- components/Card.jsx
+++ components/Card.jsx
@@ -1,10 +1,10 @@
 export const Card = ({ card, onClick }) => { /*onClick is the function datatype being passed, here handleCardClick from App.jsx*/
     //css logic for card flipping
     return (
-        <div className = {`card ${(card.isFlipped) ? "flipped" : "" }`}> 
-            <div className = "card-front" onClick = {() => onClick(card)}>?</div> 
-            {/*onClick calls the onClick function itself with the respective parameter, which is handCardClick(card)*/}
-            <div className = "card-back">{card.value}</div>
-        </div>
+    <div className={`card ${card.isFlipped ? "flipped" : ""} ${card.isMatched ? "matched" : ""}`}>
+        <div className="card-front" onClick={() => onClick(card)}>?</div>
+        {/*onClick calls the onClick function itself with the respective parameter, which is handCardClick(card)*/}
+        <div className="card-back">{card.value}</div>
+    </div>
     );
 }
